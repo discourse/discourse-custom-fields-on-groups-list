dec0da2e13b778d855b09fad56241d5607d35ecd
Jean Perez <jmperez127@gmail.com>
2023-11-07 00:28:59 -0400

    fix syntax error

--- javascripts/discourse/components/table-header-renderer.js
+++ javascripts/discourse/components/table-header-renderer.js
@@ -1,10 +1,10 @@
 import Component from "@glimmer/component";
 
 export default class TableHeaderRenderer extends Component {
+  filteredUserFields = [];
   idsToShow = settings.user_custom_field_ids_to_show
     .split("|")
     .map((id) => parseInt(id, 10));
-  };
   filteredUserFields = this.args.userFields.filter((field) =>
     this.idsToShow.includes(field.id)
   );
