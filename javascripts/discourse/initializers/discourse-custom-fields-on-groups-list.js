cd13cc466ad5c1e242202bef57026c90d80ccaac
Jarek Radosz <jarek@cvx.dev>
2026-07-13 02:54:00 +0200

    DEV: Use model extension APIs instead of modifyClass
    Uses the group-members-request value transformer instead of overriding
    Group.loadMembers via modifyClassStatic.

--- javascripts/discourse/initializers/discourse-custom-fields-on-groups-list.js
+++ javascripts/discourse/initializers/discourse-custom-fields-on-groups-list.js
@@ -1,23 +1,17 @@
-import { ajax } from "discourse/lib/ajax";
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 export const USER_FIELD_PREFIX = "user_field_";
 
 function initialize(api) {
-  const PLUGIN_ID = "discourse-custom-fields-on-groups-list";
+  api.registerValueTransformer("group-members-request", ({ value: opts }) => {
+    const params = { ...opts, include_custom_fields: true };
 
-  api.modifyClassStatic("model:group", {
-    pluginId: PLUGIN_ID,
-    loadMembers(name, opts) {
-      const params = { ...opts, include_custom_fields: true };
+    if (params.order?.startsWith(USER_FIELD_PREFIX)) {
+      params.order_field = params.order;
+      params.order = "custom_field";
+    }
 
-      if (params.order?.startsWith(USER_FIELD_PREFIX)) {
-        params.order_field = params.order;
-        params.order = "custom_field";
-      }
-
-      return ajax(`/groups/${name}/members.json`, { data: params });
-    },
+    return params;
   });
 }
 
